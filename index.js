8a06e5852355d821fd41127e1882cff4d8282e65
Mohtashim <mohtashima85@gmail.com>
2022-02-25 17:29:47 +0500

    webber project frontend

---
+++ index.js
@@ -0,0 +1,27 @@
+let nav = document.querySelector('nav'),
+    expanded = nav.getAttribute('aria-expanded'),
+    mobile_nav = document.querySelector('.mobile-nav-links'),
+    menuIcon = document.querySelector('.hotdog-icon');
+
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('show');
+    mobile_nav.classList.toggle('show');
+
+});
+window.addEventListener('resize', resizeChecker);
+window.addEventListener('DOMContentLoaded', resizeChecker);
+function resizeChecker() {
+    let width = document.body.clientWidth;
+    if (width > 768) {
+        nav.setAttribute('aria-expanded', true);
+        menuIcon.classList.remove('show');
+        menuIcon.classList.add('hide');
+    }
+    if (width < 768) {
+        if (menuIcon.classList.contains('hide')) {
+            menuIcon.classList.remove('hide');
+        }
+        nav.setAttribute('aria-expanded', false);
+    }
+}
---
+++ pages/pricing/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        colors: {
+            cynaBlue: "#1f2937",
+            blusihCyan: "#0e7490",
+            cyan: '#cffafe',
+            bondiBlue: '0891b2',
+            whiteSmoke: '#f4f4f5',
+            ebonyClay: "#1f2937",
+            dblack: "#18181b",
+            balticSea: "#27272a",
+            davyGrey: "#52525b",
+        }
+    },
+}
